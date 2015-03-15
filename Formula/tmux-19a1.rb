eb42ff49cc778547a06baea2064c95b0a04921d2
Mark Eissler <mark@mixtur.com>
2015-03-14 19:08:45 -0700

    Finalized formula.
    Also build and added bottles for:
    -Yosemite
    -Mavericks
    -Mountain Lion
    
    All bottles are on bintray.

--- Formula/tmux-19a1.rb
+++ Formula/tmux-19a1.rb
@@ -1,25 +1,20 @@
 require 'formula'
 
 class Tmux19a1 < Formula
-  homepage 'https://github.com/markeissler/homebrew-tmux-19a1'
-  url 'https://github.com/markeissler/tmux/archive/1.9a1.tar.gz'
-  sha1 '137f3907c161799f02828b603ce2e4f21b147749'
+  homepage 'https://github.com/markeissler/tmux-19a1'
+  url 'http://dl.bintray.com/markeissler/homebrew/tmux-19a1/tmux-19a1.tar.gz'
+  sha256 '2e187f02411ce6c3247f75223e62db24ae03215d8da452b4448d3fdc0d6f0d03'
 
-  # bottle do
-  #   cellar :any
-  #   revision 1
-  #   sha1 "5a5e180e33339671bc8c82ed58c26862da037f30" => :yosemite
-  #   sha1 "6092f92f5cd7eeb6ddf3b555cd4e655c4c85e826" => :mavericks
-  #   sha1 "981c8c199a2ea3df18b6651205b4616459ae1f8c" => :mountain_lion
-  # end
-
-  # force tap build vs pouring a dist bottle
-  def pour_bottle?
-    false
+  bottle do
+    root_url 'http://dl.bintray.com/markeissler/homebrew/tmux-19a1'
+    cellar :any
+    sha256 "678fc45cc513a5fe7f2c91f24de360d6f2e1a67c5a789b6ce958624b8b7f8a6b" => :yosemite
+    sha256 'b9cef372d7ca64cc2b961c625ecaa43b9361df4d0d021710add1b96aae8a019e' => :mavericks
+    sha256 "07b7932b52725d4ba3dcf5f02c654f890568827fe56d4c132752753dc4445ef6" => :mountain_lion
   end
 
   head do
-    url 'https://github.com/markeissler/homebrew-tmux-19a1.git'
+    url 'https://github.com/markeissler/tmux.git', :branch => 'me/statusline-shellcmd-var-substitution'
 
     depends_on "autoconf" => :build
     depends_on "automake" => :build
@@ -28,12 +23,6 @@ class Tmux19a1 < Formula
 
   depends_on 'pkg-config' => :build
   depends_on 'libevent'
-
-  # patch bash completion for our path?
-  # patch :p0 do
-  #   url "https://trac.macports.org/export/110576/trunk/dports/lang/gcc43/files/darwin10.diff"
-  #   sha1 "4119006a1fa18086431d3204db440750af668783"
-  # end
 
   def install
     system "sh", "autogen.sh" if build.head?
